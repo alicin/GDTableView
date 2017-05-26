f9e3f9b7064282c0b8627dae5b672357a196eeaf
Ali Can Bardakci <alicanbardakci@gmail.com>
2017-05-25 23:14:23 -0400

    Initial Commit

---
+++ GDTableView/GDTableView.h
@@ -0,0 +1,19 @@
+//
+//  GDTableView.h
+//  GDTableView
+//
+//  Created by Ali Can Bardakci on 25/05/2017.
+//  Copyright © 2017 Tavsanlar AS. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GDTableView.
+FOUNDATION_EXPORT double GDTableViewVersionNumber;
+
+//! Project version string for GDTableView.
+FOUNDATION_EXPORT const unsigned char GDTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GDTableView/PublicHeader.h>
+
+
